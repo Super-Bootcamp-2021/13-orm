670731b2087b87be953216b15ba56d8824a387a6
hudhorisan <hudhorisan@gmail.com>
2021-02-04 18:05:35 +0700

    updatedata

--- tugas/database/typeorm/main.js
+++ tugas/database/typeorm/main.js
@@ -62,10 +62,20 @@ async function deleteDataDB(con, id) {
 }
 async function getoneDB(id) {
   const worker = getConnection()
+    .getRepository(Worker)
+    .createQueryBuilder('worker')
+    .where('worker.id = :id', { id: id })
+    .getOne();
+  return worker;
+}
+async function updateDB(con, id, done) {
+  const task = con
     .createQueryBuilder()
+    .update(Task)
+    .set({ done: done })
     .where('id = :id', { id: id })
     .execute();
-  return worker;
+  return task;
 }
 async function writeDataWorker(obj) {
   try {
@@ -105,6 +115,18 @@ async function deleteData(id) {
     const conn = getConnection();
     await deleteDataDB(conn, id);
     conn.close();
+    return getoneDB(id);
+  } catch (err) {
+    console.error(err);
+  }
+
+  // getConnection().close();
+}
+async function updateData(id, done) {
+  try {
+    const conn = getConnection();
+    await updateDB(conn, id, done);
+    conn.close();
   } catch (err) {
     console.error(err);
   }
@@ -118,4 +140,5 @@ module.exports = {
   init,
   readDataTask,
   deleteData,
+  updateData,
 };
