18d0f8051f6dec46233855897c8cc766dac0713b
muhammadisan <muhammadisan2015@gmail.com>
2021-02-04 17:05:45 +0700

    worker-service sudah berjalan

--- tugas/lib/orm/entities/worker-model.js
+++ tugas/lib/orm/entities/worker-model.js
@@ -1,9 +1,14 @@
 const { EntitySchema } = require('typeorm');
 
 class Worker {
-  constructor(id, name) {
+  constructor(id, name, alamat, email, telepon, biografi, photo) {
     this.id = id;
     this.name = name;
+    this.alamat = alamat;
+    this.email = email;
+    this.telepon = telepon;
+    this.biografi = biografi;
+    this.photo = photo;
   }
 }
 
@@ -21,6 +26,26 @@ const WorkerSchema = new EntitySchema({
       type: 'varchar',
       length: 255,
     },
+    alamat: {
+      type: 'varchar',
+      length: 255,
+    },
+    email: {
+      type: 'varchar',
+      length: 255,
+    },
+    telepon: {
+      type: 'varchar',
+      length: 20,
+    },
+    biografi: {
+      type: 'varchar',
+      length: 255,
+    },
+    photo: {
+      type: 'varchar',
+      length: 255,
+    }
   },
 });
 
